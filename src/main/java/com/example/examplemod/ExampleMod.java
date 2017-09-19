44eeaeea11f2cb157b17c0cbd8f260fbb8ef08dd
Ben Klemp <isenfireldc@gmail.com>
2017-09-19 14:43:58 -0500

    This is a test of the collaborative systems, this is only a test

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -16,5 +16,6 @@ public class ExampleMod
     {
         // some example code
         System.out.println("DIRT BLOCK >> "+Blocks.DIRT.getUnlocalizedName());
+        System.out.println("This is a test of the collaborative systems, this is only a test.");
     }
 }
